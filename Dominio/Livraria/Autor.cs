1e99cd313a81bbc9ceb2cd21ab59d250ff620756
marcusruas <marcus.ruas@hotmail.com>
2019-06-24 23:39:15 -0300

    Adicionado domínio do autor

---
+++ Dominio/Livraria/Autor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Dominio.Livraria
+{
+    public class Autor
+    {
+        [Description("IDAUTOR")]
+        public int Id { get; set; }
+        [Description("NOME")]
+        public string Nome { get; set; }
+        [Description("SOBRE")]
+        public string Sobre { get; set; }
+    }
+}
