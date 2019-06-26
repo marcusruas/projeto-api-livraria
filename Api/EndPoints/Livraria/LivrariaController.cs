ab28da760dfd7088e000be7868369a9f0a503e9f
marcusruas <marcus.ruas@hotmail.com>
2019-06-25 23:59:16 -0300

    Adicionado controller da livraria

---
+++ Api/EndPoints/Livraria/LivrariaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Interface.Livraria;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.EndPoints.Livraria
+{
+    [Route("[controller]")]
+    public class LivrariaController : Controller
+    {
+        private ILivrariaServico _livrariaServico;
+        public LivrariaController(ILivrariaServico livrariaServico)
+        {
+            _livrariaServico = livrariaServico;
+        }
+
+
+    }
+}
