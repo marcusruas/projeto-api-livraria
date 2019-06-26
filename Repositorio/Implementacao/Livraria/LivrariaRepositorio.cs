1b0ae438e9066a8ddc2c51fda4feb876de10daaa
marcusruas <marcus.ruas@hotmail.com>
2019-06-25 23:39:32 -0300

    Início da implementação do repositorio

---
+++ Repositorio/Implementacao/Livraria/LivrariaRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using Repositorio.Interface.Livraria;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Implementacao.Livraria
+{
+    public class LivrariaRepositorio : ILivrariaRepositorio
+    {
+    }
+}
---
+++ Repositorio/Interface/Livraria/ILivrariaRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Interface.Livraria
+{
+    public interface ILivrariaRepositorio
+    {
+    }
+}
